3fe5cddf3c3b1c1e9e46c844414076fd62044bf4
Otavio Sonda Alenski <otavio.alenski@tivix.com>
2022-02-16 22:12:19 +0000

    feat: add .nojekyll

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/pokemon-cards",
+  assetPrefix: "/pokemon-cards/",
 };
 
 module.exports = nextConfig;
