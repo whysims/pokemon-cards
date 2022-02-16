f91334fef82fc013c586244740146bbbb7ce27aa
Otavio Sonda Alenski <otavio.alenski@tivix.com>
2022-02-16 22:40:21 +0000

    feat: add github actions

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 module.exports = nextConfig;
